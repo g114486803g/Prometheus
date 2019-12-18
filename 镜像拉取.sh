70687817612be7f7514d841600c79dc774cc36fa
g114486803g <48343071+g114486803g@users.noreply.github.com>
2019-12-18 18:49:55 +0800

    Update and rename 镜像拉取.txt to 镜像拉取.sh

--- 镜像拉取.sh
+++ 镜像拉取.sh
@@ -1,7 +1,12 @@
 docker pull registry.cn-shenzhen.aliyuncs.com/jbjb/test:prometheus
-docker pull  registry.cn-shenzhen.aliyuncs.com/jbjb/test:node-exporte
+docker pull  registry.cn-shenzhen.aliyuncs.com/jbjb/test:node-exporter
 docker pull registry.cn-shenzhen.aliyuncs.com/jbjb/test:grafana
 
 docker tag registry.cn-shenzhen.aliyuncs.com/jbjb/test:prometheus  prom/prometheus:latest
 docker tag registry.cn-shenzhen.aliyuncs.com/jbjb/test:grafana grafana/grafana:latest
 docker tag registry.cn-shenzhen.aliyuncs.com/jbjb/test:node-exporter prom/node-exporter:v0.16.0
+
+
+docker rmi registry.cn-shenzhen.aliyuncs.com/jbjb/test:prometheus
+docker rmi  registry.cn-shenzhen.aliyuncs.com/jbjb/test:node-exporter
+docker rmi registry.cn-shenzhen.aliyuncs.com/jbjb/test:grafana
